64de1241ecfd17de72bb9195e81cb49b15ebb5c7
tdcosta100 <tdcosta100@yahoo.com.br>
2011-04-04 13:28:22 -0300

    Teste

---
+++ src/com/tiago/Temp2.java
@@ -0,0 +1,13 @@
+package com.tiago;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Temp2 extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
